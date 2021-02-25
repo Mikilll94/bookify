f84871e494d25e55adcd8da5214464494dab5970
Mikołaj Waśniewski <mikolaj.wasniewski@btc-ag.com>
2021-02-25 16:33:29 +0100

    Photo dropdown

--- src/App.vue
+++ src/App.vue
@@ -27,14 +27,62 @@
         <div class="nav-bell">
           bell
         </div>
-        <img
-          class="nav-photo"
-          :src="require('@/assets/account_circle.svg')"
-        >
+        <div class="nav-photo-wrapper">
+          <div
+            @click="photoDropdownOpen = true"
+            ref="navPhoto"
+            class="nav-photo"
+          >
+            <img
+              :src="require('@/assets/account_circle.svg')"
+            >
+          </div>
+          <transition name="dropdown">
+            <div
+              v-show="photoDropdownOpen"
+              class="nav-photo-dropdown"
+              ref="photoDropdown"
+            >
+              <div class="nav-photo-dropdown__item">
+                Your profile
+              </div>
+              <div class="nav-photo-dropdown__item">
+                Settings
+              </div>
+              <div class="nav-photo-dropdown__item">
+                Sign out
+              </div>
+            </div>
+          </transition>
+        </div>
       </div>
     </nav>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      photoDropdownOpen: false,
+    };
+  },
+  mounted() {
+    document.body.addEventListener('click', (e: Event) => {
+      const navPhotoRef = this.$refs.navPhoto as HTMLElement;
+      const photoDropdownRef = this.$refs.photoDropdown as HTMLElement;
+
+      if (e.target !== this.$refs.navPhoto && e.target !== this.$refs.photoDropdown
+        && !navPhotoRef.contains(e.target as Node | null)
+        && !photoDropdownRef.contains(e.target as Node | null)) {
+        this.photoDropdownOpen = false;
+      }
+    });
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
@@ -53,6 +101,10 @@ html, body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+body {
+  height: 100vh;
 }
 
 .nav {
@@ -110,7 +162,41 @@ html, body {
   margin-right: 24px;
 }
 
+.nav-photo-wrapper {
+  position: relative;
+}
+
+.nav-photo {
+  display: flex;
+}
+
 .nav-photo:hover {
   cursor: pointer;
+}
+
+.nav-photo-dropdown {
+  position: absolute;
+  color: black;
+  margin-top: 0.5rem;
+  right: 0;
+  width: 12rem;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+  border-radius: 0.375rem;
+  background-color: white;
+  transform-origin: top right;
+}
+
+.nav-photo-dropdown__item {
+  padding: 1rem 1rem;
+  font-size: 0.875rem;
+}
+
+.dropdown-enter-active, .dropdown-leave-active {
+  transition: all ease-out 0.075s;
+}
+
+.dropdown-enter, .dropdown-leave-to {
+  opacity: 0;
+  transform: scale(0.95);
 }
 </style>
